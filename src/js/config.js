acc3d535212fe6ccf74046919fdcd0a8fc0e1ec8
Ujjawal <121127299+Ujjwal807@users.noreply.github.com>
2024-01-13 19:10:21 +0530

    config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '13b3877b-ee94-49eb-aac3-991e7e0e0bc5';
 export const MODAL_CLOSE_SEC = 2.5;
